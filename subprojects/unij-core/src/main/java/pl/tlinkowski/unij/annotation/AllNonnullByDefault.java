a37e010a81188cdf6a069c560dbb4f86ab155f02
Tomasz Linkowski <t.linkowski@gmail.com>
2018-12-31 12:15:35 +0100

    #16: added pl.tlinkowski.unij.annotation package

---
+++ subprojects/unij-core/src/main/java/pl/tlinkowski/unij/annotation/AllNonnullByDefault.java
@@ -0,0 +1,41 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2018 Tomasz Linkowski.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package pl.tlinkowski.unij.annotation;
+
+import java.lang.annotation.*;
+import javax.annotation.Nonnull;
+import javax.annotation.meta.TypeQualifierDefault;
+
+/**
+ * Declares that the return values, parameters, fields, and local variables within an annotated package are non-null by
+ * default.
+ *
+ * @author Tomasz Linkowski
+ */
+// https://youtrack.jetbrains.com/issue/KT-10942
+@Documented
+@Nonnull
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PACKAGE)
+@TypeQualifierDefault({
+        ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE,
+        ElementType.TYPE_PARAMETER, ElementType.TYPE_USE
+})
+public @interface AllNonnullByDefault {
+
+}

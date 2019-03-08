66ebe5bdc686228cf5c853c79172b771b5ec5293
Tomasz Linkowski <t.linkowski@gmail.com>
2019-03-08 12:28:16 +0100

    #22: UnmodifiableSetFactory + Jdk10UnmodifiableSetFactory

---
+++ subprojects/unij-collect-jdk10/src/main/java/pl/tlinkowski/unij/service/collect/jdk10/Jdk10UnmodifiableSetFactory.java
@@ -0,0 +1,113 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2019 Tomasz Linkowski.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package pl.tlinkowski.unij.service.collect.jdk10;
+
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import pl.tlinkowski.unij.service.UniJService;
+import pl.tlinkowski.unij.service.collect.api.UnmodifiableSetFactory;
+
+/**
+ * Implementation of {@link UnmodifiableSetFactory} that returns <a href="https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/Set.html#unmodifiable">unmodifiable
+ * sets</a> introduced in JDK 10.
+ *
+ * @author Tomasz Linkowski
+ */
+@UniJService(priority = 10)
+public final class Jdk10UnmodifiableSetFactory implements UnmodifiableSetFactory {
+
+  //region COLLECTOR
+  @Override
+  public <E> Collector<E, ?, Set<E>> collector() {
+    return Collectors.toUnmodifiableSet();
+  }
+  //endregion
+
+  //region COPY OF
+  @Override
+  public <E> Set<E> copyOf(Collection<? extends E> coll) {
+    return Set.copyOf(coll);
+  }
+  //endregion
+
+  //region OF
+  @Override
+  public <E> Set<E> of() {
+    return Set.of();
+  }
+
+  @Override
+  public <E> Set<E> of(E e1) {
+    return Set.of(e1);
+  }
+
+  @Override
+  public <E> Set<E> of(E e1, E e2) {
+    return Set.of(e1, e2);
+  }
+
+  @Override
+  public <E> Set<E> of(E e1, E e2, E e3) {
+    return Set.of(e1, e2, e3);
+  }
+
+  @Override
+  public <E> Set<E> of(E e1, E e2, E e3, E e4) {
+    return Set.of(e1, e2, e3, e4);
+  }
+
+  @Override
+  public <E> Set<E> of(E e1, E e2, E e3, E e4, E e5) {
+    return Set.of(e1, e2, e3, e4, e5);
+  }
+
+  @Override
+  public <E> Set<E> of(E e1, E e2, E e3, E e4, E e5, E e6) {
+    return Set.of(e1, e2, e3, e4, e5, e6);
+  }
+
+  @Override
+  public <E> Set<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7) {
+    return Set.of(e1, e2, e3, e4, e5, e6, e7);
+  }
+
+  @Override
+  public <E> Set<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8) {
+    return Set.of(e1, e2, e3, e4, e5, e6, e7, e8);
+  }
+
+  @Override
+  public <E> Set<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9) {
+    return Set.of(e1, e2, e3, e4, e5, e6, e7, e8, e9);
+  }
+
+  @SuppressWarnings("PMD.ExcessiveParameterSet")
+  @Override
+  public <E> Set<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9, E e10) {
+    return Set.of(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public <E> Set<E> of(E... elements) {
+    return Set.of(elements);
+  }
+  //endregion
+}
---
+++ subprojects/unij-test/src/main/groovy/pl/tlinkowski/unij/test/provider/AbstractUnmodifiableSetFactorySpec.groovy
@@ -0,0 +1,117 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2019 Tomasz Linkowski.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pl.tlinkowski.unij.test.provider
+
+import spock.lang.Shared
+import spock.lang.Specification
+
+import pl.tlinkowski.unij.service.collect.api.UnmodifiableSetFactory
+
+/**
+ * Base specification for {@link UnmodifiableSetFactory}.
+ *
+ * @author Tomasz Linkowski
+ */
+abstract class AbstractUnmodifiableSetFactorySpec extends Specification {
+
+  @Shared
+  protected UnmodifiableSetFactory factory
+
+  def "collector"(Set<Integer> set) {
+    expect:
+      set.stream().collect(factory.collector()) == set
+    where:
+      set       | _
+      []        | _
+      [1]       | _
+      [1, 2]    | _
+      [1, 2, 3] | _
+  }
+
+  def "copyOf"(Set<Integer> set) {
+    expect:
+      factory.copyOf(set) == Set.copyOf(set)
+    where:
+      set       | _
+      []        | _
+      [1]       | _
+      [1, 2]    | _
+      [1, 2, 3] | _
+  }
+
+  def "of(n=0)"() {
+    expect:
+      factory.of() == Set.of()
+  }
+
+  def "of(n=1)"() {
+    expect:
+      factory.of(1) == Set.of(1)
+  }
+
+  def "of(n=2)"() {
+    expect:
+      factory.of(1, 2) == Set.of(1, 2)
+  }
+
+  def "of(n=3)"() {
+    expect:
+      factory.of(1, 2, 3) == Set.of(1, 2, 3)
+  }
+
+  def "of(n=4)"() {
+    expect:
+      factory.of(1, 2, 3, 4) == Set.of(1, 2, 3, 4)
+  }
+
+  def "of(n=5)"() {
+    expect:
+      factory.of(1, 2, 3, 4, 5) == Set.of(1, 2, 3, 4, 5)
+  }
+
+  def "of(n=6)"() {
+    expect:
+      factory.of(1, 2, 3, 4, 5, 6) == Set.of(1, 2, 3, 4, 5, 6)
+  }
+
+  def "of(n=7)"() {
+    expect:
+      factory.of(1, 2, 3, 4, 5, 6, 7) == Set.of(1, 2, 3, 4, 5, 6, 7)
+  }
+
+  def "of(n=8)"() {
+    expect:
+      factory.of(1, 2, 3, 4, 5, 6, 7, 8) == Set.of(1, 2, 3, 4, 5, 6, 7, 8)
+  }
+
+  def "of(n=9)"() {
+    expect:
+      factory.of(1, 2, 3, 4, 5, 6, 7, 8, 9) == Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9)
+  }
+
+  def "of(n=10)"() {
+    expect:
+      factory.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) == Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  }
+
+  def "of(...)"() {
+    expect:
+      factory.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11) == Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+  }
+}

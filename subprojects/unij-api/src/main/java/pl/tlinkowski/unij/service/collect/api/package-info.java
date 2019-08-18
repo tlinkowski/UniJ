7987a87b72312214a4469f5a536e21da6f5bbe07
Tomasz Linkowski <t.linkowski@gmail.com>
2019-08-18 07:53:46 +0200

    Improve formatting in package-info.java

--- subprojects/unij-api/src/main/java/pl/tlinkowski/unij/service/collect/api/package-info.java
+++ subprojects/unij-api/src/main/java/pl/tlinkowski/unij/service/collect/api/package-info.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /**
  * UniJ {@link java.util.Collection}-factory service interfaces.
  *
--- subprojects/unij-test/src/main/groovy/pl/tlinkowski/unij/test/service/collect/package-info.java
+++ subprojects/unij-test/src/main/groovy/pl/tlinkowski/unij/test/service/collect/package-info.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /**
  * Test classes (specifications) for UniJ {@link java.util.Collection}-factory services.
  *

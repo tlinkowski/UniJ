368db444a5dba6fe21e284490f6c93bec4ddd81f
Tomasz Linkowski <t.linkowski@gmail.com>
2019-09-15 06:51:37 +0200

    Fix #51: Add `pl.tlinkowski.unij.sample.lib.usage.jdk8` and `pl.tlinkowski.unij.sample.lib.usage.jdk11`

---
+++ subprojects/samples/lib/pl.tlinkowski.unij.sample.lib.usage.jdk11/pl.tlinkowski.unij.sample.lib.usage.jdk11.gradle.kts
@@ -0,0 +1,40 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2018-2019 Tomasz Linkowski.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+  application
+}
+
+application {
+  mainClassName = "${project.name}/${project.name}.SampleLogicMain"
+}
+
+modularity.standardJavaRelease(11)
+
+dependencies {
+  implementation(project(":pl.tlinkowski.unij.sample.lib"))
+
+  implementation(project(":pl.tlinkowski.unij.service.collect.jdk10"))
+  implementation(project(":pl.tlinkowski.unij.service.misc.jdk11"))
+}
+
+tasks {
+  check {
+    dependsOn(run)
+  }
+}

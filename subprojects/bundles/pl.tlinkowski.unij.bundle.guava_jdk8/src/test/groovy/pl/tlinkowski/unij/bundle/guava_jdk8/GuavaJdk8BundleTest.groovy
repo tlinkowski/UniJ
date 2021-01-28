88366fe4c8b3394469820f14e57aebd946696fa0
Tomasz Linkowski <t.linkowski@gmail.com>
2021-01-28 13:58:58 +0100

    Update date in license headers

--- subprojects/bundles/pl.tlinkowski.unij.bundle.guava_jdk8/src/test/groovy/pl/tlinkowski/unij/bundle/guava_jdk8/GuavaJdk8BundleTest.groovy
+++ subprojects/bundles/pl.tlinkowski.unij.bundle.guava_jdk8/src/test/groovy/pl/tlinkowski/unij/bundle/guava_jdk8/GuavaJdk8BundleTest.groovy
@@ -1,7 +1,7 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  *
- * Copyright 2018-2019 Tomasz Linkowski.
+ * Copyright 2018-2021 Tomasz Linkowski.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package pl.tlinkowski.unij.bundle.guava_jdk8
 
 import pl.tlinkowski.unij.test.bundle.UniJBundleTest
